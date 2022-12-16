3fbfe9857618ee584ca19466d39a75960c4e7b19
TheProgrammingDucky <rossmcrobie@me.com>
2022-12-16 23:43:00 +0000

    Update Caption and Logo

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import pygame
 pygame.init()
 WIDTH, HEIGHT = 800, 1000
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-
+pygame.display.set_caption("Ducky Clicker")
+pygame.display.set_icon(pygame.image.load("images/duckInc logo.jpg"))
 while True:
 
     for event in pygame.event.get():
