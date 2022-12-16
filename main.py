64317fe30d503f70da03fe21e6b0a60141828d1e
BuildTools <unconfigured@null.spigotmc.org>
2022-12-16 23:36:47 +0000

    quack

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import sys
 import pygame
 
 pygame.init()
+WIDTH, HEIGHT = 800, 1000
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-print("Quack Quack Quack Quack Quack Quack Quack")
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
